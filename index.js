75f29d71b678f20c03e2b2e005aa5da6bee21f00
Abigail <abigailfayegrundy@gmail.com>
2020-07-08 15:46:56 +0100

    Adding two bus stops based on postcode

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const request = require("request");
 const readlineSync = require("readline-sync");
 
+console.log("What postcode are you at?");
+const postcode = readlineSync.prompt();
+
+request("https://api.postcodes.io/postcodes/" + postcode, function (
+  error,
+  response,
+  body
+) {
+  const returnedPostcodeBody = JSON.parse(body);
+
+  const longitudeOfPostcode = returnedPostcodeBody.result.longitude;
+  const latitudeOfPostcode = returnedPostcodeBody.result.latitude;
+
+  const tflUrl =
+    "https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" +
+    latitudeOfPostcode +
+    "&lon=" +
+    longitudeOfPostcode;
+
+  request(tflUrl, function (error, response, body) {
+    const returnedtflBody = JSON.parse(body);
+
+    const nearestTwoBusStops = returnedtflBody.stopPoints.slice(0, 2);
+
+    console.log(nearestTwoBusStops);
+  });
+});
+
 console.log("Bus Stop Code?");
 const busStopCode = readlineSync.prompt();
 
@@ -15,3 +43,5 @@ function displayFirstFive(returnedBody) {
   const firstFive = returnedBody.slice(0, 5);
   console.log(firstFive);
 }
+
+//2 BUS STOPS IDS
