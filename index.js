ad64c90f2fb8453745b0e53e54f1a47f7bf3cd45
Abigail <abigailfayegrundy@gmail.com>
2020-07-08 13:53:17 +0100

    variables renamed for clarity

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const readlineSync = require("readline-sync");
 
 console.log("Bus Stop Code?");
 const busStopCode = readlineSync.prompt();
+
 const url = "https://api.tfl.gov.uk/StopPoint/" + busStopCode + "/Arrivals";
 request(url, function (error, response, body) {
   const returnedBody = JSON.parse(body);
 
-  function firstFive(returnedBody) {
-    const fiveBack = returnedBody.slice(0, 5);
-    console.log(fiveBack);
-  }
-  firstFive(returnedBody);
+  displayFirstFive(returnedBody);
 });
+
+function displayFirstFive(returnedBody) {
+  const firstFive = returnedBody.slice(0, 5);
+  console.log(firstFive);
+}
