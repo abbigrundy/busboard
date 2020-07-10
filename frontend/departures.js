67fba95317ad055b6d646ffcb0ffb406bf54dd4e
Abigail <abigailfayegrundy@gmail.com>
2020-07-10 16:12:40 +0100

    display five busses

--- frontend/departures.js
+++ frontend/departures.js
@@ -4,4 +4,16 @@ fetch("http://localhost:3000/departureBoards?postcode=nw51tl")
   })
   .then((busStopArrivals) => {
     console.log(busStopArrivals);
+    const firstBusStopArray = extractKeyProperties(busStopArrivals.first);
+    const secondBusStopArray = extractKeyProperties(busStopArrivals.second);
+    document.querySelector("#results").innerHTML = `${firstBusStopArray.join(
+      "<br/>"
+    )}<br/><br/> ${secondBusStopArray.join("<br/>")}`;
   });
+function extractKeyProperties(busStopArrivals) {
+  return busStopArrivals.map(function (busStop) {
+    return `${busStop.destinationName}
+        ${busStop.expectedArrival}
+        ${busStop.lineName}`;
+  });
+}
